af3566a616bc71330702a9bc40763a951880fcaa
Diwakar Sharma <diwakar.sharma2k@gmail.com>
2024-08-29 22:19:10 +0400

    updated versions

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def add(x, y):
     return x + y
 
-
-result = add(1, 2)
-print(f"The sum of 1 and y is {result}")
+if __name__ == "__main__":
+    result = add(1, 2)
+    print(f"The sum of 1 and y is {result}")
